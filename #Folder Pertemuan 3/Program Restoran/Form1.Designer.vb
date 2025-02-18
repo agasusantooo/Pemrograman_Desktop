1ab1e7c16dddd0095d6105e1822f43046961f4d5
agasusantooo <117900754+agasusantooo@users.noreply.github.com>
2025-02-18 21:27:13 +0700

    Pertemuan 3

---
+++ #Folder Pertemuan 3/Program Restoran/Form1.Designer.vb
@@ -0,0 +1,320 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Label1 = New Label()
+        PictureBox1 = New PictureBox()
+        Label2 = New Label()
+        PictureBox2 = New PictureBox()
+        NumericUpDown1 = New NumericUpDown()
+        Label3 = New Label()
+        Label4 = New Label()
+        Panel2 = New Panel()
+        Label5 = New Label()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Panel1.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(NumericUpDown1, ComponentModel.ISupportInitialize).BeginInit()
+        Panel2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(PictureBox2)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(PictureBox1)
+        Panel1.Controls.Add(Button8)
+        Panel1.Controls.Add(Button7)
+        Panel1.Controls.Add(Button6)
+        Panel1.Controls.Add(Button5)
+        Panel1.Controls.Add(Button4)
+        Panel1.Controls.Add(Button3)
+        Panel1.Controls.Add(Button2)
+        Panel1.Controls.Add(Button1)
+        Panel1.Location = New Point(30, 25)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(321, 954)
+        Panel1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.FromArgb(CByte(192), CByte(255), CByte(192))
+        Button1.Location = New Point(20, 28)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(111, 111)
+        Button1.TabIndex = 0
+        Button1.Text = "MEJA 1" & vbCrLf & "2 KURSI"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        Button2.Location = New Point(183, 28)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(111, 111)
+        Button2.TabIndex = 1
+        Button2.Text = "MEJA 2" & vbCrLf & "2 KURSI"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        Button3.Location = New Point(20, 192)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(111, 111)
+        Button3.TabIndex = 2
+        Button3.Text = "MEJA 3" & vbCrLf & "4 KURSI"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        Button4.Location = New Point(183, 192)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(111, 111)
+        Button4.TabIndex = 3
+        Button4.Text = "MEJA 4" & vbCrLf & "2 KURSI"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        Button5.Location = New Point(20, 365)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(111, 111)
+        Button5.TabIndex = 4
+        Button5.Text = "MEJA 5" & vbCrLf & "6 KURSI"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = Color.FromArgb(CByte(192), CByte(255), CByte(192))
+        Button6.Location = New Point(183, 365)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(111, 111)
+        Button6.TabIndex = 5
+        Button6.Text = "MEJA 6" & vbCrLf & "4 KURSI"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = Color.FromArgb(CByte(192), CByte(255), CByte(192))
+        Button7.Location = New Point(20, 541)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(111, 111)
+        Button7.TabIndex = 6
+        Button7.Text = "MEJA 7" & vbCrLf & "8 KURSI"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = Color.FromArgb(CByte(192), CByte(255), CByte(192))
+        Button8.Location = New Point(183, 541)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(111, 111)
+        Button8.TabIndex = 7
+        Button8.Text = "MEJA 8" & vbCrLf & "6 KURSI"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(68, 703)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(33, 30)
+        Label1.TabIndex = 9
+        Label1.Text = "Isi"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        PictureBox1.Location = New Point(27, 698)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(35, 35)
+        PictureBox1.TabIndex = 8
+        PictureBox1.TabStop = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(68, 753)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(81, 30)
+        Label2.TabIndex = 11
+        Label2.Text = "Kosong"
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = Color.FromArgb(CByte(192), CByte(255), CByte(192))
+        PictureBox2.Location = New Point(27, 748)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(35, 35)
+        PictureBox2.TabIndex = 10
+        PictureBox2.TabStop = False
+        ' 
+        ' NumericUpDown1
+        ' 
+        NumericUpDown1.Location = New Point(433, 129)
+        NumericUpDown1.Name = "NumericUpDown1"
+        NumericUpDown1.Size = New Size(210, 35)
+        NumericUpDown1.TabIndex = 1
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(433, 88)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(192, 38)
+        Label3.TabIndex = 2
+        Label3.Text = "Jumlah Tamu"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(665, 129)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(243, 30)
+        Label4.TabIndex = 3
+        Label4.Text = "Maksimal Kursi Kosong 2"
+        ' 
+        ' Panel2
+        ' 
+        Panel2.Controls.Add(Button12)
+        Panel2.Controls.Add(Button11)
+        Panel2.Controls.Add(Button10)
+        Panel2.Controls.Add(Button9)
+        Panel2.Controls.Add(Label5)
+        Panel2.Location = New Point(432, 259)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(1012, 720)
+        Panel2.TabIndex = 4
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 14.1428576F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.ForeColor = Color.Black
+        Label5.Location = New Point(20, 22)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(110, 45)
+        Label5.TabIndex = 0
+        Label5.Text = "Menu"
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(27, 91)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(214, 40)
+        Button9.TabIndex = 1
+        Button9.Text = "Menu Rekomendasi"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Location = New Point(277, 91)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(214, 40)
+        Button10.TabIndex = 2
+        Button10.Text = "Makanan"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Location = New Point(531, 91)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(214, 40)
+        Button11.TabIndex = 3
+        Button11.Text = "Minuman"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.Location = New Point(775, 91)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(214, 40)
+        Button12.TabIndex = 4
+        Button12.Text = "Snack"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(12F, 30F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1510, 1008)
+        Controls.Add(Panel2)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(NumericUpDown1)
+        Controls.Add(Panel1)
+        Name = "Form1"
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(NumericUpDown1, ComponentModel.ISupportInitialize).EndInit()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button9 As Button
+
+End Class
